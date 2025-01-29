a2e3f42108d1a34bbf7a1812e9ba0b81ee3a87bb
mirz <mirza.romi@doku.com>
2025-01-29 11:48:16 +0700

    testing kafka consmer on inventory

--- inventory/src/main/java/org/mirza/inventory/InventoryApplication.java
+++ inventory/src/main/java/org/mirza/inventory/InventoryApplication.java
@@ -2,8 +2,10 @@ package org.mirza.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "org.mirza.entity")
 public class InventoryApplication {
 
     public static void main(String[] args) {
---
+++ inventory/src/main/java/org/mirza/inventory/controller/InventoryController.java
@@ -0,0 +1,11 @@
+package org.mirza.inventory.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class InventoryController {
+
+}
